e15913eaab7d32d5a23a7f244a4d9dc7e0d54113
Joonas Pessi <joonas.pessi@futurice.com>
2015-03-31 21:58:47 +0300

    Set up dev env

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import React from "react";
+import Router from "react-router";
 
-class HelloWorld extends React.Component {
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+class Home extends React.Component {
     render() {
-        return <p>Hello, world!</p>;
+        return (<div>Home</div>);
     }
 }
 
-console.log("hello");
+class Workouts extends React.Component {
+    render() {
+        return (<div>Workouts</div>);
+    }
+}
 
-React.render(
-    <HelloWorld />,
-    document.body
+class Contact extends React.Component {
+    render() {
+        return (<div>Contact</div>);
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <header className="app__header">
+                  <ul>
+                    <li><Link to="app">Home</Link></li>
+                    <li><Link to="workouts">Workouts</Link></li>
+                    <li><Link to="contact">Contact</Link></li>
+                  </ul>
+                  Welcome to JoonasPessi.com
+                </header>
+
+                <div className="app__content">
+                    <RouteHandler/>
+                </div>
+            </div>
+        )
+    }
+}
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="workouts" handler={Workouts}/>
+    <Route name="contact" handler={Contact}/>
+    <DefaultRoute handler={Home}/>
+  </Route>
 );
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
