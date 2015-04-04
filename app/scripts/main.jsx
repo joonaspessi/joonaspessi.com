922efc2ab7b32651ce7bd7455a30d20cce327b91
Joonas Pessi <joonas.pessi@futurice.com>
2015-04-04 19:59:13 +0300

    Improved sidebar styles
    * Added font-awesome icons

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -8,19 +8,25 @@ var RouteHandler = Router.RouteHandler;
 
 class Home extends React.Component {
     render() {
-        return (<div>Home</div>);
+        return (<div>Workouts</div>);
     }
 }
 
 class Workouts extends React.Component {
     render() {
-        return (<div>Workouts</div>);
+        return (<div>Workouts Data mining and graphs</div>);
     }
 }
 
 class Contact extends React.Component {
     render() {
-        return (<div className="main">Contact</div>);
+        return (<div>Contact</div>);
+    }
+}
+
+class BikeSetup extends React.Component {
+    render() {
+        return (<div>My personal bike setup</div>);
     }
 }
 
@@ -28,7 +34,7 @@ class Header extends React.Component {
     render() {
         return (
             <div className="app__header">
-                <div className="header__icon">icon</div>
+                <div className="header__icon fa fa-bars"></div>
                 <div className="header__name">JOONASPESSI</div>
             </div>
         );
@@ -40,10 +46,19 @@ class App extends React.Component {
         return (
             <div className="app">
                 <div className="app__sidebar">
-                    <ul className="app__sidebar__navigation">
-                      <li><Link to="app">Home</Link></li>
-                      <li><Link to="workouts">Workouts</Link></li>
-                      <li><Link to="bikes">Contact</Link></li>
+                    <ul className="sidebar__navigation">
+                        <li className="sidebar__item ">
+                            <Link to="app"><div className="sidebar__item__icon fa fa-home"/>Workouts</Link>
+                        </li>
+                        <li className="sidebar__item">
+                            <Link to="workouts"><div className="sidebar__item__icon fa fa-dashboard"/>Training Data</Link>
+                        </li>
+                        <li className="sidebar__item">
+                            <Link to="bikesetup"><div className="sidebar__item__icon fa fa-bicycle"/>Bike Setup</Link>
+                        </li>
+                        <li className="sidebar__item">
+                            <Link to="contact"><div className="sidebar__item__icon fa fa-diamond"/>Contact</Link>
+                        </li>
                     </ul>
                 </div>
                 <div className="app__content">
@@ -60,7 +75,8 @@ class App extends React.Component {
 var routes = (
   <Route name="app" path="/" handler={App}>
     <Route name="workouts" handler={Workouts}/>
-    <Route name="bikes" handler={Contact}/>
+    <Route name="bikesetup" handler={BikeSetup}/>
+    <Route name="contact" handler={Contact}/>
     <DefaultRoute handler={Home}/>
   </Route>
 );
