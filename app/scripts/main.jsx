f91390a3faa75dbb7c511306580d96e5c2aabbea
Joonas Pessi <joonas.pessi@futurice.com>
2015-04-04 17:13:47 +0300

    Improved application layout styles

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -20,7 +20,18 @@ class Workouts extends React.Component {
 
 class Contact extends React.Component {
     render() {
-        return (<div>Contact</div>);
+        return (<div className="main">Contact</div>);
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="app__header">
+                <div className="header__icon">icon</div>
+                <div className="header__name">JOONASPESSI</div>
+            </div>
+        );
     }
 }
 
@@ -28,17 +39,18 @@ class App extends React.Component {
     render() {
         return (
             <div className="app">
-                <header className="app__header">
-                  <ul className="app__header__navigation">
-                    <li><Link to="app">Home</Link></li>
-                    <li><Link to="workouts">Workouts</Link></li>
-                    <li><Link to="contact">Contact</Link></li>
-                  </ul>
-                  Welcome to JoonasPessi.com
-                </header>
-
+                <div className="app__sidebar">
+                    <ul className="app__sidebar__navigation">
+                      <li><Link to="app">Home</Link></li>
+                      <li><Link to="workouts">Workouts</Link></li>
+                      <li><Link to="bikes">Contact</Link></li>
+                    </ul>
+                </div>
                 <div className="app__content">
-                    <RouteHandler/>
+                    <Header/>
+                    <div className="app__main">
+                        <RouteHandler/>
+                    </div>
                 </div>
             </div>
         )
@@ -48,7 +60,7 @@ class App extends React.Component {
 var routes = (
   <Route name="app" path="/" handler={App}>
     <Route name="workouts" handler={Workouts}/>
-    <Route name="contact" handler={Contact}/>
+    <Route name="bikes" handler={Contact}/>
     <DefaultRoute handler={Home}/>
   </Route>
 );
