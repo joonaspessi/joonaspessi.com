a7718c638d0b6548ce743e8f2d3502af2d4a4770
Joonas Pessi <joonas.pessi@futurice.com>
2015-05-03 21:49:45 +0300

    Added professional and sworks image

--- app/scripts/contact.jsx
+++ app/scripts/contact.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 class Contact extends React.Component {
     render() {
-        return (<div className="contact">Twitter @joonaspessi</div>);
+        return (<div className="contact">
+                    <div className="contact__image"></div>
+                    <div className="contact__info">
+                        twitter: @joonaspessi
+                        email: rider [åt] joonaspessi.com
+                    </div>
+
+                </div>);
     }
 }
 
